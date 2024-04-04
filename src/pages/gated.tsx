5ee586d0f3e9ec5ce10c7191472a535483c7dbeb
Ben Griffin <ben.griffin@magic.link>
2024-04-04 14:53:17 -0400

    Updates to use new magic admin SDK and adding CODEOWNERS

--- src/pages/gated.tsx
+++ src/pages/gated.tsx
@@ -30,73 +30,23 @@ export default function Gated() {
       };
     }
 
-    
-    try {
-        // Call magic and validate DID token
-        const magic = new Magic(process.env.MAGIC_SECRET_KEY);
-        await magic.token.validate(didToken);
-        const { email, publicAddress: walletAddress } = await magic.users.getMetadataByToken(didToken);
-        if (!email || !walletAddress) {
-            return {
-                redirect: {
-                  destination: "/",
-                  permanent: false,
-                },
-            };
-        }
-
-        // Check on-chain if user owns NFT by calling contract with web3
-        const nftContract = process.env.NFT_CONTRACT_ADDRESS;
-        const tokenId = process.env.NFT_TOKEN_ID;
-        const web3 = new Web3(process.env.NEXT_PUBLIC_RPC_URL);
-        // Reduced ABI for ERC1155 with just balanceOf 
-        const nftContractABI = [
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "name": "_owner",
-                        "type": "address"
-                    },
-                    {
-                        "name": "_id",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                    {
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            }
-        ];
-        const contract = new web3.eth.Contract(nftContractABI, nftContract);
-        const balance = await contract.methods.balanceOf(walletAddress, tokenId).call()
-        console.log('Balance: ', balance)
-        if (balance === BigInt(0)) {
-            console.log('User does not own NFT: ', walletAddress, tokenId)
-            // User doesn't own NFT!
-            return {
-                redirect: {
-                    destination: "/accessdenied?msg=User%20does%20not%20own%20NFT",
-                  permanent: false,
-                },
-            };
-        }
-    } catch(e){
-        console.error(e);
+    // Call magic and validate DID token
+    const magic = new Magic(process.env.MAGIC_SECRET_KEY);
+    const result = await magic.utils.validateTokenOwnership(
+      didToken,
+      process.env.NFT_CONTRACT_ADDRESS,
+      'ERC1155',
+      new Web3(process.env.NEXT_PUBLIC_RPC_URL),
+      process.env.NFT_TOKEN_ID
+    )
+    if (!result.valid) {
+        console.warn(result.message)
         return {
             redirect: {
-              destination: "/accessdenied?msg=Invalid%20DID%20token%20(User%20not%20logged%20in)",
+              destination: "/accessdenied?msg=User%20does%20not%20own%20NFT",
               permanent: false,
             },
         };
-    
     }
   
     return {
--- src/pages/validate.tsx
+++ src/pages/validate.tsx
@@ -32,56 +32,18 @@ import styles from "../styles/index.module.css"
 
     
     try {
-        // Call magic and validate DID token
+        // Validate DID token
         const magic = new Magic(process.env.MAGIC_SECRET_KEY);
-        await magic.token.validate(didToken);
-        const { email, publicAddress: walletAddress } = await magic.users.getMetadataByToken(didToken);
-        if (!email || !walletAddress) {
-            return {props: {valid: false}};
-        }
-
-        // Check on-chain if user owns NFT by calling contract with web3
-        const nftContract = process.env.NFT_CONTRACT_ADDRESS;
-        const tokenId = process.env.NFT_TOKEN_ID;
-        const web3 = new Web3("https://polygon-rpc.com");
-        // Reduced ABI for ERC1155 with just balanceOf 
-        const nftContractABI = [
-            {
-                "constant": true,
-                "inputs": [
-                    {
-                        "name": "_owner",
-                        "type": "address"
-                    },
-                    {
-                        "name": "_id",
-                        "type": "uint256"
-                    }
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                    {
-                        "name": "",
-                        "type": "uint256"
-                    }
-                ],
-                "payable": false,
-                "stateMutability": "view",
-                "type": "function"
-            }
-        ];
-        const contract = new web3.eth.Contract(nftContractABI, nftContract);
-        const balance = await contract.methods.balanceOf(walletAddress, tokenId).call()
-        console.log('Balance: ', balance)
-        if (balance === BigInt(0)) {
-            console.log('User does not own NFT: ', walletAddress, tokenId)
-            // User doesn't own NFT!
-            return {props: {valid: false}};
-        }
+        const result = await magic.utils.validateTokenOwnership(
+            didToken,
+            process.env.NFT_CONTRACT_ADDRESS,
+            'ERC1155',
+            new Web3("https://polygon-rpc.com"),
+            process.env.NFT_TOKEN_ID,
+        );
+        return {props: {valid: result.valid}}
     } catch(e){
         console.error(e);
         return {props: {valid: false}};
     }
-  
-    return {props: {valid: true}};
   }
